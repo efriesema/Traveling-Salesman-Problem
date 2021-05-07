18b51f3a3aff4b623b0bf0e5b0be23d0039005d9
Ed Friesema <roninfilms01@gmail.com>
2021-05-07 08:49:24 -0700

    Ed Friesema: First complete edition

---
+++ TravelingSalesman/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿
+namespace TravelingSalesman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button9 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button10 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button11 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(324, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Points file";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Select file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 241);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 37);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Brute TSP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(324, 534);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(440, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(26, 89);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Load Set 1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(26, 131);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Load Set 2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(324, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Results";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(26, 171);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(111, 22);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Load Set 3";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(26, 294);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(111, 45);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Simulated Annealing";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(875, 44);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(541, 303);
+            this.chart1.TabIndex = 11;
+            this.chart1.Text = "chart1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(872, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Cost improvement";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(26, 359);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(111, 40);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "Genetic Algorithm";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(26, 461);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(111, 44);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "Yfantis Path";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(180, 460);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 45);
+            this.button9.TabIndex = 15;
+            this.button9.Text = "Step";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(26, 511);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(75, 17);
+            this.checkBox1.TabIndex = 16;
+            this.checkBox1.Text = "One-step?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(182, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Generate n data points";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(185, 107);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 18;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(185, 188);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 19;
+            this.button10.Text = "Create data file";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(185, 151);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(182, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Output file name";
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(26, 416);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(111, 39);
+            this.button11.TabIndex = 22;
+            this.button11.Text = "Prim\'s Apprx";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1451, 542);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Traveling Salesman";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ TravelingSalesman/Models/Utils.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelingSalesman.Models
+{ 
+    public class Node
+    {
+        public char Name { get; set; }
+        public char Parent { get; set; }
+       
+        public string Children { get; set; }
+
+        public double Value { get; set; }
+    }
+    public static class Utils
+    {
+        
+
+        public static void Permute(List<string> paths,String str,
+                               int l, int r)
+        {
+            if (l == r)
+            {
+                //Console.WriteLine(str);
+                paths.Add(str);
+            }
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = Swap(str, l, i);
+                    Permute(paths, str, l + 1, r);
+                    str = Swap(str, l, i);
+                }
+            }
+        }
+
+        public static String Swap(String a, int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+
+        public static String Reverse(String a, int x, int y)
+        {
+            //reverse the subsection of the string delineated by start and end
+            int start, end;
+            if (x > y)
+            {
+                start =y;
+                end= x;
+            } else
+            {
+                start =x;
+                end =y;
+            }
+            int width = end- start+1;
+            String sub = a.Substring(start, width);
+            String head = a.Substring(0, start);
+            String tail = a.Substring(end+1, a.Length-end-1);
+            char[] substring = sub.ToCharArray();
+            Array.Reverse(substring);
+            String rev = new string(substring); 
+            //Console.WriteLine(string.Format("original : {0}, head:{1}, rev :{2},  tail:{3}", a, head, rev, tail));
+
+            return String.Concat(head,rev,tail);
+        }
+
+        public static String Transport(String a, int x, int y, Random rand)
+        {
+            //slice out the substring and move it to some other point
+            int start, end;
+            if (x > y)
+            {
+                start = y;
+                end = x;
+            }
+            else
+            {
+                start = x;
+                end = y;
+            }
+            int width = end - start + 1;
+            String sub = a.Substring(start, width);
+            String head = a.Substring(0, start);
+            String tail = a.Substring(end + 1, a.Length - end - 1);
+            String trans = String.Concat(head, tail);
+            int insertPoint = rand.Next(trans.Length);
+            String newString = trans.Insert(insertPoint, sub);
+            //Console.WriteLine(string.Format("original : {0}, trans:{1}, sub :{2},  ne:{3}", a, trans, sub, newString));
+            return newString;
+        }
+
+        public static int Factorial(int n)
+        {
+            int ans = 1;
+            for(int i = 2; i <= n; i++)
+            {
+                ans *= i;
+            }
+            return ans;
+        }
+
+        public static void OrderedCrossover(String a, String b, int x ,int y, ref String c1, ref String c2)
+        {
+            int start, end;
+            if (x > y)
+            {
+                start = y;
+                end = x;
+            }
+            else
+            {
+                start = x;
+                end = y;
+            }
+            //mutation scheme where ti splices strings at a point and either swaps the front half or back half based on the bool
+            List<char> headA = a.Substring(0, start).ToList<char>();
+            List<char> headB = b.Substring(0, start).ToList<char>(); 
+            List<char> midA = a.Substring(start,end-start).ToList<char>(); 
+            List<char> midB = b.Substring(start,end-start).ToList<char>();
+            List<char> tailA = a.Substring(end).ToList<char>();
+            List<char> tailB = b.Substring(end).ToList<char>();
+
+            List<char> C1 = new List<char> ();
+            List<char> C2 = new List<char>();
+            List<char> removeC1 = new List<char>();
+            List<char> removeC2 = new List<char>();
+            List<char> newTailC1 = new List<char>();
+            List<char> newTailC2 = new List<char>();
+            List<char> newHeadC1 = new List<char>();
+            List<char> newHeadC2 = new List<char>();
+            //Console.WriteLine(String.Format("original a:{0}, b:{1} start:{2}, end{3}",a,b,start,end));
+            //Console.WriteLine(String.Format("headA :{0}, midA:{1}, tailA:{2}", PrintList(headA), PrintList(midA), PrintList(tailA)));
+            //Console.WriteLine(String.Format("headB :{0}, midB:{1}, tailB:{2}", PrintList(headB), PrintList(midB), PrintList(tailB)));
+            //Build first child
+
+            //start with midB list
+            //take midB list and remove any common letters with midA put into remove list
+            //take midA and remove any common chars with midB
+            removeC1.AddRange(midB);
+            removeC2.AddRange(midA);
+            newHeadC1.AddRange(headA);
+            newHeadC2.AddRange(headB);
+            newTailC1.AddRange(tailA);
+            newTailC2.AddRange(tailB);
+            foreach ( char letter in IntersectList(midA, midB))
+            {
+                removeC1.Remove(letter);
+                removeC2.Remove(letter);
+
+            }
+            newHeadC1.AddRange(removeC2);
+            newHeadC2.AddRange(removeC1);
+            foreach( char letter in removeC1){
+                newHeadC1.Remove(letter);
+                newTailC1.Remove(letter);
+            }
+            foreach (char letter in removeC2)
+            {
+                newHeadC2.Remove(letter);
+                newTailC2.Remove(letter);
+            }
+            //Remove any items in removeC1 from newHeadC1 and add them to newTailC1
+            if (newTailC1.Count < tailA.Count)
+            {
+                //Console.WriteLine("C1 Tail is shorter by {0}", tailA.Count - newTailC1.Count);
+
+                for (int i =0; i< tailA.Count-newTailC1.Count; i++)
+                {
+                    char temp = newHeadC1[0];
+                    newHeadC1.Remove(temp);
+                    newTailC1.Add(temp);
+                }
+            }
+            if(newTailC2.Count < tailB.Count)
+            {
+                //Console.WriteLine("C2 Tail is shorter by {0}", tailB.Count - newTailC2.Count);
+                for (int i = 0; i < tailB.Count - newTailC2.Count; i++)
+                {
+                    char temp = newHeadC2[0];
+                    newHeadC2.Remove(temp);
+                    newTailC2.Add(temp);
+                }
+            }
+            C1.AddRange(newHeadC1);
+            C1.AddRange(midB);
+            C1.AddRange(newTailC1);
+            //Console.WriteLine(String.Format("C1:new head :{0}, mid : {1}, new tail :{2}", PrintList(newHeadC1), PrintList(midB), PrintList(newTailC1)));
+
+            C2.AddRange(newHeadC2);
+            C2.AddRange(midA);
+            C2.AddRange(newTailC2);
+            //Console.WriteLine(String.Format("C2:new head :{0}, mid : {1}, new tail :{2}", PrintList(newHeadC2), PrintList(midA), PrintList(newTailC2)));
+
+            c1 = PrintList(C1);
+            c2 = PrintList(C2);
+           // Console.WriteLine(String.Format("a:{0} ,b : {1}, c1 : {2}, c2:{3}", a, b, c1,c2));
+
+        }
+
+        public static string PrintList(List<char> cList)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(char c in cList)
+            {
+                sb.Append(c);
+            }
+            return sb.ToString();
+        }
+
+        public static List<char> IntersectList(List<char>A, List<char> B)
+        {
+            //Return a list of all chars contained in both lists
+            List<char> bothContain = new List<char>();
+            foreach (char c in A)
+            {
+                if (B.Contains(c))
+                {
+                    bothContain.Add(c);
+                }  
+            }
+            return bothContain;
+        }
+
+        public static String SwapMutation(String a , double p, Random rand)
+        {
+            //Mutate a string with each charcter having a possibility, p, of flipping with the next letter
+            StringBuilder sb = new StringBuilder(a);
+            double coin;
+            for(int i=0; i < a.Length - 1; i++)
+            {
+                coin = rand.NextDouble();
+                if(p > coin)
+                {
+                    char temp = sb[i];
+                    sb[i] = sb[i + 1];
+                    sb[i+1] = temp;
+                }
+            }
+
+            return sb.ToString();
+        }
+
+
+    }
+}
